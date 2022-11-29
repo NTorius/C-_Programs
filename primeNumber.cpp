1f485aeceb9d75488ead5d68a26fb78360197bd9
NTorius <nihal.tonpe2018@gmail.com>
2022-11-29 22:16:01 +0530

    Check The pime number c++ program

---
+++ primeNumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   cout << "Enter the number to check is prime or not";
+    int a;
+    cin >> a;
+    bool b = true;
+    for(int i =1; i<a; i++){
+        if(a % i == 0){
+            b=false;
+        }
+    }
+    if(b== false){
+        cout << "Enter the number is prime number";
+        return 0;
+    }
+    cout << "Enter the number is not prime number";
+    return 0;
+}
